a125f83ffc683d8a794c84c4031ef325b83d799f
kimsungchan <dlrj1234@naver.com>
2018-06-14 09:29:07 +0900

    1차 만보기 구성

--- app/src/main/java/com/example/inha/androidpj/WalkCountActivity.java
+++ app/src/main/java/com/example/inha/androidpj/WalkCountActivity.java
@@ -1,15 +1,94 @@
 package com.example.inha.androidpj;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
+import android.widget.EditText;
 import android.view.View;
+import android.view.Window;
 
-public class WalkCountActivity extends AppCompatActivity {
+public class WalkCountActivity extends AppCompatActivity implements SensorEventListener {
+
+    private Handler wHandler;
+    public static int cnt = 0;
+    private EditText edtstreet;
+    private EditText edtcal;
+    private EditText edtwalk;
+    private TextView txtTime;
+
+    private long lastTime;
+    private float speed;
+    private float lastX;
+    private float lastY;
+    private float lastZ;
+    private float x, y, z;
+
+    private static final int SHAKE_THRESHOLD = 750;
+    private static final int DATA_X = SensorManager.DATA_X;
+    private static final int DATA_Y = SensorManager.DATA_Y;
+    private static final int DATA_Z = SensorManager.DATA_Z;
+
+    private SensorManager sensorManager;
+    private Sensor accelerormeterSensor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_walk_count);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerormeterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        edtstreet = (EditText)findViewById(R.id.edtstreet);
+        edtcal = (EditText)findViewById(R.id.edtcal);
+        edtwalk = (EditText)findViewById(R.id.edtwalk);
+
+        edtwalk.setText("" +cnt);
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        if(accelerormeterSensor != null)
+            sensorManager.registerListener(this,accelerormeterSensor,SensorManager.SENSOR_DELAY_GAME);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (sensorManager != null)
+            sensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long currentTime = System.currentTimeMillis();
+            long gabOfTime = (currentTime - lastTime);
+            if (gabOfTime > 100) {
+                lastTime = currentTime;
+                x = event.values[SensorManager.DATA_X];
+                y = event.values[SensorManager.DATA_Y];
+                z = event.values[SensorManager.DATA_Z];
+
+                speed = Math.abs(x + y + z - lastX - lastY - lastZ) / gabOfTime * 10000;
+
+                if (speed > SHAKE_THRESHOLD) {
+                    edtwalk.setText("" + (++cnt));
+                }
+
+                lastX = event.values[DATA_X];
+                lastY = event.values[DATA_Y];
+                lastZ = event.values[DATA_Z];
+            }
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
     public void onBtnFinishClicked(View view){
