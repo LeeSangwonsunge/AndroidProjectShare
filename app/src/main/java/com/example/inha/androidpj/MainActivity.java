331fcfbcb41dac2423a4899aa2db1c501fb22800
Sangwon <zzxng123@naver.com>
2018-06-18 17:49:13 +0900

    Add Calendar

--- app/src/main/java/com/example/inha/androidpj/MainActivity.java
+++ app/src/main/java/com/example/inha/androidpj/MainActivity.java
@@ -7,19 +7,43 @@ import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private GPSService gpsService;
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.Calendar_btn){
+            Intent intent = new Intent(this, Calendar.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startService(new Intent(this, GPSService.class));
+
+        Intent incomingIntent = getIntent();
+        String date = incomingIntent.getStringExtra("date");
+        Toast.makeText(this, date, Toast.LENGTH_LONG).show();
     }
 
     public void onBtnWalkCountClicked(View view){
